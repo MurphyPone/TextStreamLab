de923f4a020ffc8068209666b946277af9f94ae8
MurphyP1 <MurphyP1@104-10-2017.bi.local>
2017-09-19 12:10:33 -0400

    Postlab

--- TextStreamLab/src/textTester.java
+++ TextStreamLab/src/textTester.java
@@ -3,15 +3,15 @@ import java.io.*;
 import java.util.Scanner;
 
 public class textTester {
-	//Method that returns scanner of words.txt
-	//Method that returns a PrintWriter of ramblex dictionary.java
-	
-	//NEED TO MAKE A FORMATTIG METHOD WHICH WILL BE SENT INTO THE RAMBLEX FILE
-	
+	// Method that returns scanner of words.txt
+	// Method that returns a PrintWriter of ramblex dictionary.java
+
+	// NEED TO MAKE A FORMATTIG METHOD WHICH WILL BE SENT INTO THE RAMBLEX FILE
+
 	public static Scanner openWords(String fname) {
 		File file = new File(fname);
-		Scanner input = null; 
-				
+		Scanner input = null;
+
 		try {
 			input = new Scanner(file);
 		} catch (FileNotFoundException ex) {
@@ -21,11 +21,11 @@ public class textTester {
 		
 		return input;
 	}
-	
+
 	public static PrintWriter openDictionary(String fname) {
 		File file = new File(fname);
-		PrintWriter output = null; 
-				
+		PrintWriter output = null;
+
 		try {
 			output = new PrintWriter(file);
 		} catch (FileNotFoundException ex) {
@@ -34,18 +34,41 @@ public class textTester {
 		}
 		return output;
 	}
+
+	public static void writeEntry(Scanner input, PrintWriter output) {
+		while (input.hasNextLine()) {
+			String word = input.nextLine().toUpperCase(); // Smart Programming!
+
+			if (word.length() >= 3 && word.length() <= 5) {
+				output.println("\t\t\"" + word + "\",");
+			}
+		}
+	}
 	
+	public static void writeEntryHeader(PrintWriter output) {
+		output.println("public class RamblexDictionary { \n\tprivate String[] words = {");
+	}
+	public static void writeEntryFooter(PrintWriter output) {
+		output.println("\t};\n}");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if (args.length < 2) {  //Need two args, input and output files
+		if (args.length < 2) { // Need two args, input and output files
 			System.out.println("No file given, not enough args");
 			System.exit(1);
 		}
-		Scanner in = openWords(args[0]);  //First argument passed in via command line
-		if (in ==null) System.exit(1);;		
-		
+		Scanner in = openWords(args[0]); // First argument passed in via command line
+		if (in == null)
+			System.exit(1);
+		;
+
 		PrintWriter out = openDictionary(args[1]);
-		out.println(in.nextLine());
+
+		writeEntryHeader(out);
+		writeEntry(in, out);
+		writeEntryFooter(out);
+		
 		in.close();
 		out.close();
 	}
