809c8e5ae131ee44f386ea3df061fd3f1ac8296a
MurphyP1 <MurphyP1@104-10-2017.bi.local>
2017-09-19 11:15:35 -0400

    pre lunch lunc

--- TextStreamLab/src/textTester.java
+++ TextStreamLab/src/textTester.java
@@ -1,10 +1,12 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Scanner;
 
 public class textTester {
 	//Method that returns scanner of words.txt
 	//Method that returns a PrintWriter of ramblex dictionary.java
+	
+	//NEED TO MAKE A FORMATTIG METHOD WHICH WILL BE SENT INTO THE RAMBLEX FILE
 	
 	public static Scanner openWords(String fname) {
 		File file = new File(fname);
@@ -20,14 +22,31 @@ public class textTester {
 		return input;
 	}
 	
+	public static PrintWriter openDictionary(String fname) {
+		File file = new File(fname);
+		PrintWriter output = null; 
+				
+		try {
+			output = new PrintWriter(file);
+		} catch (FileNotFoundException ex) {
+			System.out.println("error u goob, no can do bc of " + ex.toString());
+			return null;
+		}
+		return output;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if (args.length < 1) {
+		if (args.length < 2) {  //Need two args, input and output files
 			System.out.println("No file given, not enough args");
 			System.exit(1);
 		}
-		Scanner in = openWords(args[0]);
-		System.out.println(in.nextLine());
+		Scanner in = openWords(args[0]);  //First argument passed in via command line
+		if (in ==null) System.exit(1);;		
+		
+		PrintWriter out = openDictionary(args[1]);
+		out.println(in.nextLine());
 		in.close();
+		out.close();
 	}
 }
